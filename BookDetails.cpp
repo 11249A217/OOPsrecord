896a80335415f0ca89ed1cac8e812047c9e1918b
11249A217 <11249a217@kanchiuniv.ac.in>
2025-11-04 23:05:12 -0800

    Implement Book class and display book details
    Fixed variable name typo and added missing newline.

---
+++ BookDetails.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class Book{
+    public:
+    string title;
+    string author;
+    float price;
+    void display(){
+        cout<<"Title:"<<title
+        <<",Author:"<<author
+        <<",Price:"<<price<<endl;
+    }
+};
+int main(){
+    Book b1,b2;
+    b1.title="C++ programming";
+    b1.author="Bjarne Stroustrup";
+    b3.price=750.50;
+    b2.title="Data Structures";
+    b2.author="Mark Allen Weiss";
+    b2.price=600.75;
+    b1.display();
+    b2.display();
+    return 0;
+}
